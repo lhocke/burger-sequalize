784621ea82b99601cdbf2226a7c8ab741534122d
lhocke <dylan.ishihara@gmail.com>
2017-07-29 12:43:35 -0700

    adds burger and customer routes

--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,22 +1,38 @@
 var db = requires("../models");
 
 module.exports = function(app) {
-	app.get("/api/burger", function(req, res) {
-		db.Burger.findAll({}).then(function(dbBurger) {
-			res.json(dbBurger)
-		});
-	});
+	app.get("/api/burgers", function(req, res) {
+		var query= {};
+		if (req.query.customer_id) {
+			query.CustomerId = req.query.customer_id;
+		}
 
-	app.post("/api/burger", function(req, res) {
-		db.Burger.create({
-			test: req.body.text,
-			complete: req.body.complete
+		db.Customer.findAll({
+			where: query,
+			include: [db.Customer]
 		}).then(function(dbBurger) {
 			res.json(dbBurger);
 		});
 	});
 
-	app.delete("/api/burger/:id", function(req, res) {
+	app.get("/api/burgers/:id", function(req, res) {
+		db.Burger.findOne({
+			where: {
+				id: req.params.id
+			},
+			include: [db.Customer]
+		}).then(function(dbBurger){
+			res.json(dbBurger)
+		})
+	});
+
+	app.post("/api/burgers", function(req, res) {
+		db.Burger.create(req.body).then(function(dbBurger) {
+			res.json(dbBurger);
+		});
+	});
+
+	app.delete("/api/burgers/:id", function(req, res) {
 		db.Burger.destroy({
 			where: {
 				id: req.params.id
@@ -26,6 +42,16 @@ module.exports = function(app) {
 		});
 	});
 
-	app.put("/api/Burger", function(req, res))
+	app.put("/api/burgers", function(req, res) {
+		db.Burger.update(
+			req.body,
+			{
+				where: {
+					id: req.body.id
+				}
+			}).then(function(dbBurger) {
+				res.json(dbBurger);
+			});
+	});
 
-}
+};
