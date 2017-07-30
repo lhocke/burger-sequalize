961bd8cc3945cec91736bbc1d7ceb22c43ad80d3
lhocke <dylan.ishihara@gmail.com>
2017-07-30 15:20:53 -0700

    afternoon backup

--- server.js
+++ server.js
@@ -28,3 +28,8 @@ app.use("/customers", customerRoute);
 
 app.use("/", './routes/html-routes');
 
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
